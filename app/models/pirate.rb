afc1281623fbea78534b48c708afd310b2526b3f
github username <github email address>
2018-06-05 10:55:22 -0400

    to have_content was not working even though data was being displayed

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@all = Array.new
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
