a79b2e5c5b46b36541fca4842ff233c637d514b9
prmoreira23 <prm.gredes@gmail.com>
2018-06-06 16:39:55 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,25 @@
+require 'pry'
 class Pirate
+  attr_accessor :name, :weight, :height, :ships
+
+  @@all = []
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @ships = params[:ships].collect {|ship| Ship.new(ship) }
+    @@all << self
+  end
+
+  def add_ship(ship)
+    ships << ship
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
 end
