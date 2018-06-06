7c909f6844e510269abc7e685300db618de6b64e
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-06-06 16:37:33 -0400

    Completed sinatra-nested-forms-dumbo-web-051418

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+  
+  attr_accessor :name, :weight, :height
+
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+    @@all << self
+  end
+
 end
